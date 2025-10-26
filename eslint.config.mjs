65bf97964b150c79f08cbb33b9c175b0a146233d
enjidev <me@enji.dev>
2025-10-27 00:22:42 +0700

    feat: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default withNuxt(
+  {
+    ignores: ['worker-configuration.d.ts'],
+  },
+  eslintPluginPrettierRecommended,
+);
