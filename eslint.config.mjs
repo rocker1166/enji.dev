b4f08e8abc499699375a3f80915622a287d090a3
enjidev <me@enji.dev>
2025-10-27 00:50:58 +0700

    chore: add import sort rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,34 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // side effect imports
+            ['^\\u0000'],
+            // `node:` built-in modules
+            ['^node:'],
+            // packages
+            ['^@?\\w'],
+            // absolute imports and other imports such as vue-style `@/foo`
+            ['^'],
+            // relative imports
+            ['^\\.'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
     ignores: ['worker-configuration.d.ts'],
   },
   eslintPluginPrettierRecommended,
