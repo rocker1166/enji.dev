1ad77440b6ab87b9a84da847c799e29a6a5130f8
enjidev <me@enji.dev>
2025-10-27 01:25:16 +0700

    chore: change dev server port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  devServer: {
+    port: 4321,
+  },
   nitro: {
     preset: 'cloudflare_module',
-
     cloudflare: {
       deployConfig: true,
       nodeCompat: true,
     },
   },
-
   modules: ['nitro-cloudflare-dev', '@nuxt/eslint'],
 });
